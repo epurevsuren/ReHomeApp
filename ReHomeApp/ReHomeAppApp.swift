7a355e3159dcd994b36f09886e47cc5b93fa02c8
ShivamArora99 <Shivam.Arora@student.uts.edu.au>
2024-05-31 13:31:15 +1000

    add: initialised project

---
+++ ReHomeApp/ReHomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReHomeAppApp.swift
+//  ReHomeApp
+//
+//  Created by Shivam Arora on 30/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReHomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

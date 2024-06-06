4f0e13d25c5c93f70030d0cbc314bb5ec0674722
Purevsuren Erdene (Peter) <purevsuren.er@gmail.com>
2024-06-06 13:55:21 +1000

    Improved

--- ReHomeApp/UserStoryView.swift
+++ ReHomeApp/UserStoryView.swift
@@ -12,6 +12,7 @@ struct UserStoryView: View {
     let title: String
     
     var body: some View {
+        /*
         VStack(alignment: .center) {
             Image("p1")
                 .resizable()
@@ -29,6 +30,37 @@ struct UserStoryView: View {
                 .padding()
                 .scaledToFit()
         }
+         */
+        
+        VStack(alignment: .leading, spacing: 8) {
+            ZStack {}
+            .frame(width: 100, height: 100)
+            .background(
+              Image(imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .scaledToFit()
+                .clipped()
+            )
+            .background(Color(red: 0.96, green: 0.96, blue: 0.96))
+            .cornerRadius(48)
+            
+            VStack(alignment: .leading, spacing: 0) {
+                Text(title)
+                  .font(
+                    Font.custom("Inter", size: 14)
+                      .weight(.medium)
+                  )
+                  .multilineTextAlignment(.center)
+                  .foregroundColor(.black)
+                  .frame(width: 100, alignment: .center)
+            }
+            .padding(0)
+            .frame(width: 100, alignment: .topLeading)
+            
+        }
+        .padding(0)
+        
     }
 }
 
